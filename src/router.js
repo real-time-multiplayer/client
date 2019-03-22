8309b25e5f2ba8463d898f5f5a674f68f68db946
resta.adiputra <resta.adiputra@gmail.com>
2019-03-22 12:07:32 +0800

    room for user done, add only, no delete

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Landing from './views/Landing.vue';
 
 Vue.use(Router);
 
@@ -11,7 +12,15 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue'),
+      component: Landing,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
   ],
 });
